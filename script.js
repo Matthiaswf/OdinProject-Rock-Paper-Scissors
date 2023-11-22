bcd1560526dfc66630ba929727a08715b7d3990c
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-22 15:22:19 +0900

    Basic Win determination and 1 message complete

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-// Creating an Array for the options Player and PC Choose from
+// An Array for the options Player and PC Choose from
 const choices = ["Rock", "Paper", "Scissors"];
 
-// Creating the "Game" choosing one of the three randomly
+// Function for the "Game" choosing one of the three randomly
+
+let computer = undefined;
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * choices.length);
-    return(computerChoice, choices[computerChoice]);
+    computerChoice = (computerChoice, choices[computerChoice]);
+    computer = computerChoice;
+    return computer;
 }
 
 console.log(getComputerChoice())
 
-// Creating the function for nthe players choice
+// The function for the players choice
+
+let player =  undefined;
 
 function getPlayerChoice(){
     let playerChoice = Math.floor(Math.random() * choices.length);
-    return(playerChoice, choices[playerChoice]);
+    playerChoice = (playerChoice, choices[playerChoice]);
+    player = playerChoice;
+    return player;
 }
 
 console.log(getPlayerChoice())
+
+// The function top determine the outcome and log the apropiate message
+
+const winMessage = "You Win!";
+const lossMessage = "You Lose!";
+const drawMessage = "It's a Draw!";
+let resultMessage;
+
+function determineWinner(){
+
+    if ( computer === 'Rock' && player === 'Scissors'){
+        resultMessage = lossMessage;
+        console.log(resultMessage);
+    }
+}
+
+determineWinner()
