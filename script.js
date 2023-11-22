05a1df9d97bd2d922555c12542cb53370c661fc4
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-22 16:35:02 +0900

    Score being added at wins and scores printed

--- script.js
+++ script.js
@@ -40,50 +40,77 @@ function getPlayerChoice(){
 console.log("You chose " + getPlayerChoice() + "!")
 */
 
-// The function top determine the outcome and log the apropiate message
+// The function top determine the outcome and log the apropiate message. It also increments the scores.
 
 const winMessage = "You Win!";
 const lossMessage = "You Lose!";
 const drawMessage = "It's a Draw!";
 let resultMessage;
 
+let playerScore = 0;
+let computerScore = 0;
+
 function determineWinner(){
 
     if ( computer === 'Rock' && player === 'Scissors'){
         resultMessage = lossMessage;
+        computerScore +=1;
         console.log(resultMessage + " " + computer + " beats " + player + '!');
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Paper' && player === 'Rock'){
         resultMessage = lossMessage;
+        computerScore +=1;
         console.log(resultMessage + " " + computer + " beats " + player + '!');
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Scissors' && player === 'Paper'){
         resultMessage = lossMessage;
+        computerScore +=1;
         console.log(resultMessage + " " + computer + " beats " + player + '!');
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Scissors' && player === 'Rock'){
         resultMessage = winMessage;
+        playerScore +=1;
         console.log(resultMessage + " " + player + " beats " + computer + '!');
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Rock' && player === 'Paper'){
         resultMessage = winMessage;
+        playerScore +=1;
         console.log(resultMessage + " " + player + " beats " + computer + '!');
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Paper' && player === 'Scissors'){
         resultMessage = winMessage;
+        playerScore +=1;
         console.log(resultMessage + " " + player + " beats " + computer + '!');
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Scissors' && player === 'Scissors'){
         resultMessage = drawMessage;
         console.log(resultMessage);
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Rock' && player === 'Rock'){
         resultMessage = drawMessage;
         console.log(resultMessage);
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
     else if ( computer === 'Paper' && player === 'Paper'){
         resultMessage = drawMessage;
         console.log(resultMessage);
+        console.log("Computer: " + computerScore);
+        console.log("Player: " + playerScore);
     }
 }    
 determineWinner();
