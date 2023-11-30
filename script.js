ac5b09f8ef3daa1a2aaad9af7ab1ef8f4514c1db
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-30 17:32:27 +0900

    Added the random computer choice function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const choices = ['rock', 'paper', 'scissors'];
+let computerChoice = undefined;
+
 let result = document.getElementById("result");
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', determineWinner);
-    
+
+function getComputerChoice(){
+    const computerChoice = Math.floor(Math.random() * choices.length);
+    return(computerChoice, choices[computerChoice]);
+}
 function determineWinner() {
 result.textContent = 'You Win!';
 }
