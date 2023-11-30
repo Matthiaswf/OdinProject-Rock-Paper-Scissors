126e85b9d8517bcbcf0d2ec58ca74d30efa241ba
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-11-30 17:42:14 +0900

    All buttons added and tested via log

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['Rock', 'Paper', 'Scissors'];
 let computerChoice = undefined;
+let playerChoice = undefined;
 
 let result = document.getElementById("result");
 
 const rock = document.querySelector('#rock');
-rock.addEventListener('click', determineWinner);
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+
+
+
+
+rock.addEventListener('click', () => {
+    console.log('rockBtnClicked');
+    playerChoice = 'Rock';
+    determineWinner();
+});
+paper.addEventListener('click', () => {
+    console.log('paperBtnClicked');
+    playerChoice = 'Paper';
+    determineWinner();
+});
+scissors.addEventListener('click', () => {
+    console.log('scissorsBtnClicked');
+    playerChoice = 'Scissors';
+    determineWinner();
+});
+
+
+
 
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random() * choices.length);
+
     return(computerChoice, choices[computerChoice]);
 }
+
 function determineWinner() {
+
 result.textContent = 'You Win!';
 }
